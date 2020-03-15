1066793ea277970a85e66ce1312466e660c9dc5f
zzz20002026 <56061912+zzz20002026@users.noreply.github.com>
2020-03-15 22:04:00 +0800

    Add files via upload

--- Main2.java
+++ Main2.java
@@ -1,29 +1,31 @@
+/**
+ * Main2
+ */
 public class Main2 {
 
     public static void main(String[] args) {
-        Chess chess1 = new Chess();
-        chess1.setname("Chess");
-        chess1.setcolor("black and red");
-        chess1.setamount("32");
-        chess1.setmaterial("wood");
-        chess1.setdevelopment("Sengoku");
-        chess1.showinfo();
+        Student student1 = new Student();
+        student1.setName("Frank");
+        student1.setId("108021085");
+        student1.setGender("male");
+        student1.setAge("17");
+        student1.setSign("Aries");
+        student1.showinfo();
         System.out.println();
-        Chess chess2 = new Chess();
-        chess2.setname("Chess");
-        chess2.setcolor("white and black");
-        chess2.setamount("32");
-        chess2.setmaterial("wood");
-        chess1.setdevelopment("India");
-        chess2.showinfo();
+        Student student2 = new Student();
+        student2.setName("Shark");
+        student2.setId("108021160");
+        student2.setGender("male");
+        student2.setAge("17");
+        student2.setSign("Leo");
+        student2.showinfo();
         System.out.println();
-        Chess chess3 = new Chess();
-        chess3.setname("Checkers");
-        chess3.setcolor("green and red and yellow");
-        chess3.setamount("256");
-        chess3.setmaterial("plastic");
-        chess3.setdevelopment("China");
-        chess3.showinfo();
-        System.out.println();
+        Student student3 = new Student();
+        student3.setName("Elsa");
+        student3.setId("108021184");
+        student3.setGender("female");
+        student3.setAge("17");
+        student3.setSign("Aries");
+        student3.showinfo();
     }
 }
